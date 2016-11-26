dc068e1d967288a2f871abf72afefa1e02a12dda
Jan Hartman <jan.hartman7@gmail.com>
2016-11-26 20:40:51 +0100

    added sidebar

--- js/code.js
+++ js/code.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", loadTasks);
-
+window.onload = function() {
+    window.document.body.onload = load();
+};
 
 
 function addTask(parent, child) {
@@ -13,7 +14,6 @@ function doneTask(element) {
 
 }
 
-
 function loadJSON(file, callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
@@ -26,27 +26,47 @@ function loadJSON(file, callback) {
     xobj.send(null);
 }
 
-function loadTasks() {
+function load() {
+    if (document.querySelector(".tasks"))
+        loadTasks(showTasks);
+    else if (document.querySelector(("#graph")))
+        drawGraph();
+}
+
+function showTasks(tasks) {
     var taskFormat = "<li class=\"task\"> <header>{0}</header> <p>Priority: {1}</p> <p>Due: {2}</p> <p>Description: {3}</p> <button>Edit</button> <button onclick=\"doneTask(this)\">Done</button> </li>";
 
-    loadJSON("js/tasks.json", function(data) {
-        var tasks = JSON.parse(data);
-        var categoryTasksList = document.getElementsByClassName("categoryTasksList")[0];
-        var dailyTasksList = document.getElementsByClassName("dailyTasksList")[0];
+    var categoryTasksList = document.getElementsByClassName("categoryTasksList")[0];
+    var dailyTasksList = document.getElementsByClassName("dailyTasksList")[0];
 
-        tasks.tasks.forEach(function(task) {
-            var taskNode = document.createElement("LI");
-            var taskNode2 = document.createElement("LI");
-            taskNode.innerHTML = taskNode2.innerHTML = taskFormat.format(task.name, task.priority, task.due, task.description);
-            addTask(categoryTasksList, taskNode);
-            addTask(dailyTasksList, taskNode2);
-        })
+    tasks.forEach(function(task) {
+        var taskNode = document.createElement("LI");
+        var taskNode2 = document.createElement("LI");
+        taskNode.innerHTML = taskNode2.innerHTML = taskFormat.format(task.name, task.priority, task.due, task.description);
+        addTask(categoryTasksList, taskNode);
+        addTask(dailyTasksList, taskNode2);
     })
+}
+
+function loadTasks(callback) {
+    loadJSON("js/tasks.json", function (data) {
+        var tasks = JSON.parse(data).tasks;
+        callback(tasks);
+    });
 
 }
 
-String.prototype.format = function()
-{
+function drawGraph() {
+    var c = document.getElementById("graph");
+    var ctx = c.getContext("2d")
+
+
+
+    ctx.stroke();
+
+}
+
+String.prototype.format = function() {
    var content = this;
    for (var i = 0; i < arguments.length; i++) {
         var replacement = '{' + i + '}';
@@ -55,7 +75,20 @@ String.prototype.format = function()
    return content;
 };
 
-/*
+/* HIDE SHOW CATEGORIES */
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    /*document.getElementById("main").style.marginLeft = "250px";*/
 
- */
+    document.getElementById('openCat').style.visibility = 'hidden';
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    /*document.getElementById("main").style.marginLeft= "0";*/
+
+    document.getElementById('openCat').style.visibility = 'visible';
+}
+
